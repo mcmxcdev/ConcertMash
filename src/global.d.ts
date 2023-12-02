85f816e76fdf79897ae737073ded98e789997628
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2023-12-02 16:49:34 -0700

    chore: project maintenance
    - improve eslint and lint-staged config
    - remove check:types command which is already handled by validate command
    - upgrade pnpm and node versions used through volta
    - upgrade all deps to latest except for vite

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="@sveltejs/kit" />
 
 declare module 'svelte-filepond';
+declare module 'svelte-awesome';
 declare module 'filepond-plugin-image-exif-orientation';
 declare module 'filepond-plugin-image-preview';
