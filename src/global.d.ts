decd90d1606114a1d8b86ded64b25502d9ac4796
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2023-12-16 08:16:25 -0700

    chore: project maintenance
    - upgrade all dependencies to latest
    - upgrade pnpm from 8.11.0 to 8.12.1
    - move tsconfig.paths to kit.alias
    - add svelte-simple-modal to stub type definitions in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,5 +2,6 @@
 
 declare module 'svelte-filepond';
 declare module 'svelte-awesome';
+declare module 'svelte-simple-modal';
 declare module 'filepond-plugin-image-exif-orientation';
 declare module 'filepond-plugin-image-preview';
