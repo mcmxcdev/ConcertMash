fb745797ad3107199039f0563ed2bec1239f9494
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2024-02-17 13:51:35 -0700

    chore: project maintenance
    - upgrade all dependencies to latest
    - remove stub module declarations from global.d.ts
    - update pre-commit file due to husky v9 upgrade
    - change husky install command to husky only in prepare script

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="@sveltejs/kit" />
 
 declare module 'svelte-filepond';
-declare module 'svelte-awesome';
-declare module 'svelte-simple-modal';
 declare module 'filepond-plugin-image-exif-orientation';
 declare module 'filepond-plugin-image-preview';
