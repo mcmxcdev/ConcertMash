5448a5ae7f6b267907c9a7f0a58a5199e42fb77a
MCMXC <mcmxcdev@protonmail.com>
2025-12-09 06:08:46 -0700

    chore: project maintenance update (#24)
    - upgrade dependencies
    - sync up `.gitignore`, `tsconfig.json` and `prettier.config.js` to SvelteKit starter template
    - change from `localhost:3000` to `127.0.0.1:3000` due to updated Spotify requirements
    - use `resolve` and `SvelteURL` imports from `@svelte/kit` where needed

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,11 @@
+<script>
+  import { resolve } from '$app/paths';
+</script>
+
 <header class="border-spotify-green w-full border-b-4 bg-white">
   <div class="container mx-auto px-3 py-3">
     <div class="flex justify-between">
-      <a href="/" class="text-xl font-semibold"> ConcertMash </a>
+      <a href={resolve('/')} class="text-xl font-semibold"> ConcertMash </a>
     </div>
   </div>
 </header>
