d19c56ebe74ae91ce646a31ddef6b7c4527006b7
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2026-06-18 16:46:23 -0600

    try another app code fix

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { replaceState } from '$app/navigation';
+  import { resolve } from '$app/paths';
   import PlaylistCreationForm from '$lib/PlaylistCreationForm.svelte';
 
   export let data: {
     user: SpotifyApi.CurrentUsersProfileResponse;
   };
+
+  onMount(() => {
+    if (new URL(globalThis.location.href).searchParams.has('code')) {
+      replaceState(resolve('/app'), {});
+    }
+  });
 </script>
 
 <svelte:head>
